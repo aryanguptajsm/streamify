61f3680de89ef2e1add5c8e909f7a74a3ec5b257
Aryan gupta <aryangupta.jsm@gmail.com>
2026-06-19 23:51:42 +0530

    feat: add animated-background component and integrate react-player

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
-=======
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":false}';
->>>>>>> 9a11eceedd14e8328bac75c5bda3dff6a435679c
--- .next/server/server-reference-manifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
->>>>>>> 9a11eceedd14e8328bac75c5bda3dff6a435679c
